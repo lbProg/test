0423f92389030790360076635a0a75e0ac77ccd5
lbProg <77025660+lbProg@users.noreply.github.com>
2021-01-07 21:03:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 
-app.use(express.static('public'));
+app.use(express.static('index.html'));
 
 console.log('server running');
 
